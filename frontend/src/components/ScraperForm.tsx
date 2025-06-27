8f5b64ae2d49a0ac08d225d0f50dc81dcb07f503
ronny8dmxxx <ronnydiaz23@gmail.com>
2025-06-27 22:43:15 +0100

    Update API base URL in ScraperForm to point to the correct server

--- frontend/src/components/ScraperForm.tsx
+++ frontend/src/components/ScraperForm.tsx
@@ -13,7 +13,7 @@ import { Alert, AlertDescription } from "@/components/ui/alert"
 import { useNavigate } from "react-router-dom"
 
 const apiClient = axios.create({
-  baseURL: 'https://superscrape-116e3847ada7.herokuapp.com',
+  baseURL: 'http://ec2-3-8-194-108.eu-west-2.compute.amazonaws.com',
   timeout: 300000,
   headers: {
     'Content-Type': 'application/json',
